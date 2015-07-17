cecda04ea69e4cb13d7fec8c900090378ac1c7a1
Yen-Chia Hsu <legenddolphin@gmail.com>
2015-07-16 23:29:07 -0400

    Added grayish level feature.

--- evalSmoke.m
+++ evalSmoke.m
@@ -10,7 +10,7 @@ tile_path = '1/2/2.mp4';
 % read ground truth and responses
 target_dir = 'frames';
 path = fullfile(target_dir,date_path,dataset_path,tile_path);
-load(fullfile(path,'label.mat'));
+load(fullfile(path,'label_black_smoke.mat'));
 load(fullfile(path,'feature_smoke.mat'));
 
 % read mask
@@ -22,24 +22,24 @@ day_max_idx = 14000;
 
 % count the number of smoke pixels in each images and rescale to 0~1
 sum_smoke_pixel = sum(reshape(label(bbox_row,bbox_col,:,:),[],size(label,4)));
-sum_smoke_pixel = sum_smoke_pixel - min(sum_smoke_pixel(:));
-sum_smoke_pixel = sum_smoke_pixel/max(abs(sum_smoke_pixel(:)));
+% sum_smoke_pixel = sum_smoke_pixel - min(sum_smoke_pixel(:));
+% sum_smoke_pixel = sum_smoke_pixel/max(abs(sum_smoke_pixel(:)));
 
 % process features
 fields = fieldnames(feature);
 for i=1:length(fields)
     % rescale all features between 0 and 1
     vec = feature.(fields{i});
-    vec = vec - min(vec(day_min_idx:day_max_idx));
-    vec = vec/max(abs(vec(day_min_idx:day_max_idx)));
+%     vec = vec - min(vec(day_min_idx:day_max_idx));
+%     vec = vec/max(abs(vec(day_min_idx:day_max_idx)));
     % apply smoothing
-    vec = filter1D(vec,2);
+%     vec = filter1D(vec,2);
     % find peaks
-    min_peak_prominence = 0.15;
+    min_peak_prominence = 0.2*max(vec(:));
     min_peak_height = 0;
     min_peak_distance = 0;
     thr = 0;
-    max_peak_width = 1000;
+    max_peak_width = 0;
     [pks,locs,w,p] = findpeaks(vec,'MinPeakProminence',min_peak_prominence,'MinPeakHeight',min_peak_height,'MinPeakDistance',min_peak_distance,'Threshold',thr,'MaxPeakWidth',max_peak_width);
     % prediction
     predict = false(size(vec));
@@ -64,18 +64,30 @@ figure(98)
 subplot(img_rows,img_cols,1)
 bar(sum_smoke_pixel,'r')
 xlim([day_min_idx day_max_idx])
-set(gca,'YTickLabel',[]);
-set(gca,'YTick',[]);
 title(['Distribution of Smoke ( ',date_path,dataset_path,tile_path,' )'])
 
 subplot(img_rows,img_cols,2)
 plot(feature.img_bs_mask_clean.vec,'b')
 xlim([day_min_idx day_max_idx])
-set(gca,'YTickLabel',[]);
-set(gca,'YTick',[]);
 title('Background subtraction (10 min = 120 frames)')
 hold on
-plot(feature.img_bs_60.locs,feature.img_bs_60.pks,'ro')
+plot(feature.img_bs_mask_clean.locs,feature.img_bs_mask_clean.pks,'ro')
+hold off
+
+subplot(img_rows,img_cols,3)
+plot(feature.gray_level_mean.vec,'b')
+xlim([day_min_idx day_max_idx])
+title('Grayish level mean')
+hold on
+plot(feature.gray_level_mean.locs,feature.gray_level_mean.pks,'ro')
+hold off
+
+subplot(img_rows,img_cols,4)
+plot(feature.gray_level_std.vec,'b')
+xlim([day_min_idx day_max_idx])
+title('Grayish level std')
+hold on
+plot(feature.gray_level_std.locs,feature.gray_level_mean.pks,'ro')
 hold off
 
 % plot prediction
@@ -94,3 +106,17 @@ xlim([day_min_idx day_max_idx])
 set(gca,'YTickLabel',[]);
 set(gca,'YTick',[]);
 title('Background subtraction (10 min = 120 frames)')
+
+subplot(img_rows,img_cols,3)
+bar(uint8(feature.gray_level_mean.predict),'b')
+xlim([day_min_idx day_max_idx])
+set(gca,'YTickLabel',[]);
+set(gca,'YTick',[]);
+title('Grayish level mean')
+
+subplot(img_rows,img_cols,4)
+bar(uint8(feature.gray_level_std.predict),'b')
+xlim([day_min_idx day_max_idx])
+set(gca,'YTickLabel',[]);
+set(gca,'YTick',[]);
+title('Grayish level std')
--- util/detectSmoke.m
+++ util/detectSmoke.m
@@ -32,6 +32,19 @@ function [ responses,imgs_filtered ] = detectSmoke( img,img_bg )
     img_bs_mask_clean = removeSmallRegions(img_bs_mask_clean,100);
     img_bs_mask_clean = removeNoise(img_bs_mask_clean);
     imgs_filtered.img_bs_mask_clean = img_bs_mask_clean;
-    responses.img_bs_mask_clean = sum(imgs_filtered.img_bs_mask_clean(:));   
+    responses.img_bs_mask_clean = sum(imgs_filtered.img_bs_mask_clean(:));
+    
+    % compute grayish level of the original image in the mask
+    r_vec = img_smooth(:,:,1);
+    r_vec = r_vec(img_bs_mask_clean==1);
+    g_vec = img_smooth(:,:,2);
+    g_vec = g_vec(img_bs_mask_clean==1);
+    b_vec = img_smooth(:,:,3);
+    b_vec = b_vec(img_bs_mask_clean==1);
+    gray_level = double(abs(r_vec-g_vec)+abs(g_vec-b_vec)+abs(b_vec-r_vec));
+    gray_level_mean = mean(gray_level);
+    gray_level_std = std(gray_level);
+    responses.gray_level_mean = gray_level_mean;
+    responses.gray_level_std = gray_level_std;
 end
 
