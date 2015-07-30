1773eb6e99bc8735de6d4148f1420d446ccfe070
Yen-Chia Hsu <legenddolphin@gmail.com>
2015-07-30 15:02:18 -0400

    Added gpuArray for computing median images.

--- batchMedianImage.m
+++ batchMedianImage.m
@@ -2,9 +2,10 @@ tic
 clear all;
 addpath(genpath('libs'));
 addpath(genpath('util'));
+use_gpu_array = false;
 
 % set data source
-date_path = '2015-05-02.timemachine/';
+date_path = '2015-05-01.timemachine/';
 dataset_path = 'crf26-12fps-1424x800/';
 tile_path = '1/2/2.mp4';
 
@@ -14,23 +15,31 @@ path = fullfile(target_dir,date_path,dataset_path,tile_path);
 data_mat = matfile(fullfile(path,'data.mat'));
 
 % create workers
-try
-    fprintf('Closing any pools...\n');
-    delete(gcp('nocreate'));
-catch ME
-    disp(ME.message);
+if(~use_gpu_array)
+    try
+        fprintf('Closing any pools...\n');
+        delete(gcp('nocreate'));
+    catch ME
+        disp(ME.message);
+    end
+    local_cluster = parcluster('local');
+    num_workers = 3;
+    parpool('local',num_workers);
 end
-local_cluster = parcluster('local');
-num_workers = 3;
-parpool('local',num_workers);
 
 % compute median images over a time period
 % ranges = [60,120,360,720]; % 5min,10min,30min,60min
 ranges = [60]; % 5min
 for j=1:length(ranges)
     median = zeros(size(data_mat,'data'),'uint8');
-    parfor i=1:3
-        median(:,:,i) = computeMedian(squeeze(data_mat.data(:,:,i,:)),ranges(j));
+    if(use_gpu_array)
+        for i=1:3
+            median(:,:,i,:) = computeMedian(squeeze(data_mat.data(:,:,i,:)),ranges(j),use_gpu_array);
+        end
+    else
+        parfor i=1:3
+            median(:,:,i,:) = computeMedian(squeeze(data_mat.data(:,:,i,:)),ranges(j),use_gpu_array);
+        end
     end
     % save file
     filename = ['data_median_',num2str(ranges(j)),'.mat'];
@@ -39,5 +48,7 @@ for j=1:length(ranges)
 end
 
 % close workers
-delete(gcp('nocreate'));
+if(~use_gpu_array)
+    delete(gcp('nocreate'));
+end
 toc
--- util/computeMedian.m
+++ util/computeMedian.m
@@ -1,8 +1,12 @@
-function imgs_median = computeMedian( imgs,range )
+function imgs_median = computeMedian( imgs,range,use_gpu_array )
     num_frames = size(imgs,3);
     imgs_median = zeros(size(imgs,1),size(imgs,2),num_frames,'uint8');
     ptr_end = range;
-    pool = imgs(:,:,1:range);
+    if(use_gpu_array)
+        pool = gpuArray(imgs(:,:,1:range));
+    else
+        pool = imgs(:,:,1:range);
+    end
     for t=1:num_frames
         fprintf('Processing frame %d using range = %d\n',t,range);
         if t > range && t < num_frames-range+1
@@ -10,6 +14,10 @@ function imgs_median = computeMedian( imgs,range )
             pool = circshift(pool,-1,3);
             pool(:,:,end) = imgs(:,:,ptr_end);
         end
-        imgs_median(:,:,t) = mean(pool,3);
+        if(use_gpu_array)
+            imgs_median(:,:,t) = gather(mean(pool,3));
+        else
+            imgs_median(:,:,t) = mean(pool,3);
+        end
     end
 end
