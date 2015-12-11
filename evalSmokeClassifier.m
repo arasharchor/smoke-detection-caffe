ed2934c92356dbd71f8a9ebe49dfce28ca9c605d
Yen-Chia Hsu <legenddolphin@gmail.com>
2015-12-11 15:52:45 -0500

    Added code for evaluating the smoke classifier.

--- evalSmokeClassifier.m
+++ evalSmokeClassifier.m
@@ -27,6 +27,38 @@ for idx=1:numel(date)
         load(fullfile(path,'label_simple.mat'));
         load(fullfile(path,'label_predict_classifier.mat'));
         
+        % find local max
+        min_peak_prominence = 1;
+        min_peak_height = 1;
+        min_peak_distance = 0;
+        thr = 0;
+        min_peak_width = 1;
+        max_peak_width = 6;
+        [pks,locs,w,p] = findpeaks(label_predict_classifier,'MinPeakProminence',min_peak_prominence,'MinPeakHeight',min_peak_height,'MinPeakDistance',min_peak_distance,'Threshold',thr,'MaxPeakWidth',max_peak_width,'MinPeakWidth',min_peak_width);
+
+        % remove night time idx
+        idx_remove = find(locs<sunrise_frame | locs>sunset_frame);
+        pks(idx_remove) = [];
+        locs(idx_remove) = [];
+        w(idx_remove) = [];
+        p(idx_remove) = [];
+
+        % prediction
+        predict = false(size(label_predict_classifier));
+        for j=1:length(locs)
+            w_ = w(j)*2;
+            seg_start = round(locs(j)-w_);
+            seg_end = round(locs(j)+w_);
+            if(seg_start < 1)
+                seg_start = 1;
+            end
+            if(seg_end > numel(predict))
+                seg_end = numel(predict);
+            end
+            predict(seg_start:seg_end) = true;
+        end
+        predict = double(predict);
+        
         if(plot_result)
             label_simple = double(label_simple);
             label_simple(1:sunrise_frame) = 0;
@@ -37,20 +69,32 @@ for idx=1:numel(date)
             label_simple_plot(label_simple_plot>1) = 1;
             label_simple_plot(label_simple_plot<0) = 0;
             
+            % compute F-score
+            [fscore,predict_merged] = computeFscore(label_simple>=smoke_level,predict);
+            fscore.date = date{idx}
+            
             % plot ground truth and prediction
             fig = figure(105);
             fig_idx = 1;
             img_cols = 1;
-            img_rows = 2;
+            img_rows = 3;
 
             subplot(img_rows,img_cols,fig_idx)
-            bar(label_predict_classifier,'b')
+            plot(label_predict_classifier,'b')
+            xlim([sunrise_frame sunset_frame])
+            title('Response of smoke detection')
+            hold on
+            plot(locs,pks,'ro')
+            hold off
+            fig_idx = fig_idx + 1;
+
+            subplot(img_rows,img_cols,fig_idx)
+            bar(predict_merged,'b')
             xlim([sunrise_frame sunset_frame])
             set(gca,'YTickLabel',[]);
             set(gca,'YTick',[]);
             round_to = 2;
-%             fscore_str = ['(',num2str(round(fscore.precision,round_to)),', ',num2str(round(fscore.recall,round_to)),', ',num2str(round(fscore.score,round_to)),')'];
-            fscore_str = '';
+            fscore_str = ['(',num2str(round(fscore.precision,round_to)),', ',num2str(round(fscore.recall,round_to)),', ',num2str(round(fscore.score,round_to)),')'];
             title(['Predicted frames: (precision, recall, fscore) = ',fscore_str])
             fig_idx = fig_idx + 1;
 
@@ -62,8 +106,14 @@ for idx=1:numel(date)
             title(['Ground truth ( ',date{idx},' )'])
             fig_idx = fig_idx + 1;
             
-        end
-            
+            % print figure
+            print_dir = 'figs';
+            if ~exist(print_dir,'dir')
+                mkdir(print_dir);
+            end
+            set(gcf,'PaperPositionMode','auto')
+            print(fig,fullfile(print_dir,date{idx}),'-dpng','-r0')
+        end   
     catch ME
         fprintf('Error detecting smoke of date %s\n',date{idx});
         logError(ME);
